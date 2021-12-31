7b4f46336e9b587ce61b01c2a8fc70a5d1b21a1b
Lucas Belin <lucasbelinpro@gmail.com>
2021-12-31 18:28:47 +0100

    init solidjs and tailwind project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div class="flex h-screen items-center justify-center">
+      <h1 class="text-green-500 text-3xl">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
