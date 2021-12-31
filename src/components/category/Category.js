fb5b75b323e001e9a41c119c898317b4bf1ac723
Lucas Belin <lucasbelinpro@gmail.com>
2021-12-31 19:49:00 +0100

    add progress bar to category

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react"
+import { ProgressBar } from ".."
+
+const PROGRESS_BAR_START_PX = 17
+const PROGRESS_BAR_END_PX = 170
+const PROGRESS_BAR_START_PERCENT = 0
 
 function Category({ id, name, accent, tasks }) {
   const [taskCount, setTaskCount] = useState(tasks.length)
   const [taskCompletedCount, setTaskCompletedCount] = useState(tasks.filter((task) => task.completed).length)
 
+  const [progressPercent, setProgressPercent] = useState(PROGRESS_BAR_START_PERCENT + "%")
+  const [progressBarPos, setProgressBarPos] = useState(PROGRESS_BAR_START_PX + "px")
+
   useEffect(() => {
     const completed = tasks.filter((task) => task.completed).length
     setTaskCount(tasks.length)
     setTaskCompletedCount(completed)
+    setProgressPercent(getProgressPercentage())
+    setProgressBarPos(getProgressBarPosition())
     // eslint-disable-next-line
   }, [tasks])
 
@@ -15,6 +25,18 @@ function Category({ id, name, accent, tasks }) {
     if (tasks.length === 0) return ""
     const progress = (taskCompletedCount / taskCount) * 100
     return `(${Math.round(progress)}%)`
+  }
+
+  function getProgressPercentage() {
+    if (tasks.length === 0) return "0%"
+    return (taskCompletedCount / taskCount) * 100 + "%"
+  }
+
+  function getProgressBarPosition() {
+    if (tasks.length === 0) return PROGRESS_BAR_START_PX + "px"
+    if (taskCompletedCount === tasks.length) return PROGRESS_BAR_END_PX - 1 + "px"
+    const val = (taskCompletedCount / taskCount) * (PROGRESS_BAR_END_PX - (PROGRESS_BAR_START_PX - 1)) + (PROGRESS_BAR_START_PX - 1)
+    return Math.max(PROGRESS_BAR_START_PX, val)
   }
 
   return (
@@ -25,7 +47,7 @@ function Category({ id, name, accent, tasks }) {
       </p>
       <h2 className="text-white capitalize truncate mb-5 text-lg select-none">{name}</h2>
 
-      <div className="w-full border border-pink"></div>
+      <ProgressBar progressBarPos={progressBarPos} progressPercent={progressPercent} accent={accent} />
     </div>
   )
 }
---
+++ src/components/progress-bar/ProgressBar.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+function ProgressBar({ progressPercent, progressBarPos, accent }) {
+  return (
+    <div>
+      <div className="relative">
+        <div className="overflow-hidden h-2px text-xs flex rounded bg-progress">
+          <div
+            style={{ width: progressPercent, backgroundColor: accent }}
+            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center transition-all"
+          ></div>
+        </div>
+      </div>
+
+      <div
+        style={{ backgroundColor: accent, left: progressBarPos, clipPath: "polygon(50% 0, 100% 100%, 0 100%)" }}
+        className="h-2 w-1.5 bg-red-500 absolute transform -translate-y-2.4 rotate-180 transition-all"
+      ></div>
+    </div>
+  )
+}
+
+export default ProgressBar
