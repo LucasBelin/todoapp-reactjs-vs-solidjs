6d3c16daa57a37e3b038e1217cf738e9dd9436e6
Lucas Belin <lucasbelinpro@gmail.com>
2022-01-09 23:24:37 +0100

    refactor category component

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -8,62 +8,64 @@ const PROGRESS_BAR_START_PX = 17
 const PROGRESS_BAR_END_PX = 170
 const PROGRESS_BAR_START_PERCENT = 0
 
-function Category({ id, name, accent, tasks, updateCategory, openCategoryDetails }) {
-  const [taskCount, setTaskCount] = useState(tasks.length)
-  const [taskCompletedCount, setTaskCompletedCount] = useState(tasks.filter((task) => task.completed).length)
+function Category({ category, updateCategory, openCategoryDetails }) {
+  const [taskCount, setTaskCount] = useState(category.tasks.length)
+  const [taskCompletedCount, setTaskCompletedCount] = useState(category.tasks.filter((task) => task.completed).length)
 
   const [progressPercent, setProgressPercent] = useState(PROGRESS_BAR_START_PERCENT + "%")
   const [progressBarPos, setProgressBarPos] = useState(PROGRESS_BAR_START_PX + "px")
 
   const handleOpenCategoryDetails = useCallback(() => {
-    openCategoryDetails({ id, name, tasks })
-  }, [id, name, tasks, openCategoryDetails])
+    openCategoryDetails(category)
+  }, [category, openCategoryDetails])
+
+  const getProgressPercentageRounded = useCallback(() => {
+    if (category.tasks.length === 0) return ""
+    const progress = (taskCompletedCount / taskCount) * 100
+    return `(${Math.round(progress)}%)`
+  }, [category.tasks, taskCompletedCount, taskCount])
+
+  const getProgressPercentage = useCallback(() => {
+    if (category.tasks.length === 0) return "0%"
+    return (taskCompletedCount / taskCount) * 100 + "%"
+  }, [category.tasks, taskCompletedCount, taskCount])
+
+  const getProgressBarPosition = useCallback(() => {
+    if (category.tasks.length === 0) return PROGRESS_BAR_START_PX + "px"
+    if (taskCompletedCount === category.tasks.length) return PROGRESS_BAR_END_PX - 1 + "px"
+    const progressPx = (taskCompletedCount / taskCount) * (PROGRESS_BAR_END_PX - (PROGRESS_BAR_START_PX - 1)) + (PROGRESS_BAR_START_PX - 1)
+    return Math.max(PROGRESS_BAR_START_PX, progressPx)
+  }, [category.tasks, taskCompletedCount, taskCount])
 
   useEffect(() => {
-    const completed = tasks.filter((task) => task.completed).length
-    setTaskCount(tasks.length)
+    const completed = category.tasks.filter((task) => task.completed).length
+    setTaskCount(category.tasks.length)
     setTaskCompletedCount(completed)
     setProgressPercent(getProgressPercentage())
     setProgressBarPos(getProgressBarPosition())
-    // eslint-disable-next-line
-  }, [tasks])
+  }, [category.tasks, getProgressPercentage, getProgressBarPosition])
 
-  function getProgressPercentageRounded() {
-    if (tasks.length === 0) return ""
-    const progress = (taskCompletedCount / taskCount) * 100
-    return `(${Math.round(progress)}%)`
-  }
-
-  function getProgressPercentage() {
-    if (tasks.length === 0) return "0%"
-    return (taskCompletedCount / taskCount) * 100 + "%"
-  }
-
-  function getProgressBarPosition() {
-    if (tasks.length === 0) return PROGRESS_BAR_START_PX + "px"
-    if (taskCompletedCount === tasks.length) return PROGRESS_BAR_END_PX - 1 + "px"
-    const progressPx = (taskCompletedCount / taskCount) * (PROGRESS_BAR_END_PX - (PROGRESS_BAR_START_PX - 1)) + (PROGRESS_BAR_START_PX - 1)
-    return Math.max(PROGRESS_BAR_START_PX, progressPx)
-  }
-
-  function changeAccent(e) {
-    e.stopPropagation()
-    const nextAccent = getNextAccent(accent)
-    updateCategory({ id, name, accent: nextAccent, tasks })
-  }
+  const changeAccent = useCallback(
+    (e) => {
+      e.stopPropagation()
+      const nextAccent = getNextAccent(category.accent)
+      updateCategory({ id: category.id, name: category.name, accent: nextAccent, tasks: category.tasks })
+    },
+    [category, updateCategory],
+  )
 
   return (
     <div
       onClick={handleOpenCategoryDetails}
       className="bg-darkBlue transition-all hover:bg-darkBlueHover rounded-xl shadow-lg px-5 py-4  w-48 relative flex-shrink-0"
     >
-      <div onClick={changeAccent} style={{ backgroundColor: accent }} className="h-3 w-3 rounded-full absolute right-2 top-2"></div>
+      <div onClick={changeAccent} style={{ backgroundColor: category.accent }} className="h-3 w-3 rounded-full absolute right-2 top-2"></div>
       <p className="text-fontAlt text-sm mb-1 select-none">
         {taskCount} {taskCount > 1 ? "tasks" : "task"} {getProgressPercentageRounded()}
       </p>
-      <h2 className="text-white capitalize truncate mb-5 text-lg select-none">{name}</h2>
+      <h2 className="text-white capitalize truncate mb-5 text-lg select-none">{category.name}</h2>
 
-      <ProgressBar progressBarPos={progressBarPos} progressPercent={progressPercent} accent={accent} />
+      <ProgressBar progressBarPos={progressBarPos} progressPercent={progressPercent} accent={category.accent} />
     </div>
   )
 }
--- src/containers/categories/Categories.js
+++ src/containers/categories/Categories.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useCallback } from "react"
 
-import { Category, AddModal } from "../../components"
+import { Category, ModalAdd } from "../../components"
 import { useToggle } from "../../custom-hooks"
 import { getAccent } from "../../utils"
 
@@ -8,27 +8,38 @@ import { AiOutlinePlus } from "react-icons/ai"
 import { v4 as uuid } from "uuid"
 
 function Categories({ data, setData, openCategoryDetails }) {
-  const [addCategoryModalIsOpen, toggleAddCategoryModalIsOpen] = useToggle(false)
+  const [modalAddCategoryIsOpen, toggleModalAddCategoryIsOpen] = useToggle(false)
 
-  function updateCategory(category) {
-    const categoryIdx = data.findIndex((cat) => cat.id === category.id)
-    data[categoryIdx] = category
-    setData([...data])
-  }
+  const handleCloseModal = useCallback(() => {
+    toggleModalAddCategoryIsOpen(false)
+  }, [toggleModalAddCategoryIsOpen])
 
-  function addCategory(name) {
-    if (name.length === 0) return
-    const newCategory = { id: uuid(), name: name, accent: getAccent(data.length), tasks: [] }
-    data.push(newCategory)
-    setData([...data])
-  }
+  const handleUpdateCategory = useCallback(
+    (category) => {
+      const categoryIdx = data.findIndex((cat) => cat.id === category.id)
+      data[categoryIdx] = category
+      setData([...data])
+    },
+    [data, setData],
+  )
+
+  const handleAddCategory = useCallback(
+    (name) => {
+      if (name.length === 0) return
+      const newCategory = { id: uuid(), name: name, accent: getAccent(data.length), tasks: [] }
+      data.push(newCategory)
+      setData([...data])
+      toggleModalAddCategoryIsOpen(false)
+    },
+    [data, setData, toggleModalAddCategoryIsOpen],
+  )
 
   return (
     <div className="flex flex-col self-stretch">
       <div className="flex justify-between items-center">
         <h2 className="select-none uppercase text-fontAlt font-semibold tracking-wider">Categories</h2>
         <span
-          onClick={() => toggleAddCategoryModalIsOpen(true)}
+          onClick={() => toggleModalAddCategoryIsOpen(true)}
           className="bg-darkBlue hover:bg-darkBlueHover text-fontAlt text-sm rounded-full px-3 py-1 select-none"
         >
           Add category
@@ -37,7 +48,7 @@ function Categories({ data, setData, openCategoryDetails }) {
 
       {data.length === 0 && (
         <div
-          onClick={() => toggleAddCategoryModalIsOpen(true)}
+          onClick={() => toggleModalAddCategoryIsOpen(true)}
           className="flex gap-2 items-center self-start bg-darkBlue transition-all hover:bg-darkBlueHover rounded-xl shadow-lg px-4 py-2 mt-3"
         >
           <AiOutlinePlus color="white" size={22} />
@@ -50,23 +61,16 @@ function Categories({ data, setData, openCategoryDetails }) {
           return (
             <Category
               key={category.id}
-              id={category.id}
-              name={category.name}
-              tasks={[...category.tasks]}
+              category={{ ...category, tasks: [...category.tasks] }}
               accent={category.accent}
-              updateCategory={updateCategory}
+              updateCategory={handleUpdateCategory}
               openCategoryDetails={openCategoryDetails}
             />
           )
         })}
       </div>
 
-      <AddModal
-        isOpen={addCategoryModalIsOpen}
-        closeModal={() => toggleAddCategoryModalIsOpen(false)}
-        inputPlaceholder="Category name"
-        callback={addCategory}
-      />
+      <ModalAdd isOpen={modalAddCategoryIsOpen} closeModal={handleCloseModal} inputPlaceholder="Category name" callback={handleAddCategory} />
     </div>
   )
 }
