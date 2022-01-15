6d1998ea51fcf5471cb60e1dd5b4feffb95801b8
Lucas Belin <lucasbelinpro@gmail.com>
2022-01-15 21:05:02 +0100

    finish header container

---
+++ src/components/separator/Separator.jsx
@@ -0,0 +1,5 @@
+function Separator({ opacity }) {
+  return <div style={{ opacity: opacity }} className="h-px bg-white rounded-full w-full"></div>
+}
+
+export default Separator
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,24 @@
+import { createMemo } from "solid-js"
+import { data } from "../../stores/store"
+import { Separator } from "../../components"
+
 function Header() {
+  const pendingTaskCount = createMemo(() => {
+    let count = 0
+    data().forEach((category) => {
+      category.tasks.forEach((task) => {
+        if (!task.completed) count++
+      })
+    })
+    return count
+  })
+
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="flex flex-col gap-8 justify-center items-center w-full">
+      <h1 className="text-2xl select-none text-white font-bold self-center text-center">
+        You have <span className="text-pink select-none">{pendingTaskCount}</span> pending {pendingTaskCount > 1 ? "tasks" : "task"}
+      </h1>
+      <Separator />
     </div>
   )
 }
